ad5f46299c55d8a1c08becfdb1ff110c8ede0b77
Joseph Edusei <jedusei99@gmail.com>
2020-04-06 16:05:51 +0000

    Initial commit.

---
+++ main.js
@@ -0,0 +1,20 @@
+// Initialize and configure app
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to database and start listening for requests
+const mongoose = require('mongoose');
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (!err)
+            app.listen(process.env.PORT || 2020, () => console.log("Application is live."));
+        else {
+            console.error("An error occurred while attempting to establish a connection to the database.");
+            console.error(err);
+        }
+    }
+);
