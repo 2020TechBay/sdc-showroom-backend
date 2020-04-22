7189018120b71dd3c5c0913240d0e1320f66c73f
Joseph Edusei <jedusei99@gmail.com>
2020-04-22 15:32:26 +0000

    Added error handler and fixed mongoose deprecation message

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require('./Routers').init(app);
+// Error handler
+app.use((err, req, res, next) => {
+    res.sendStatus(500);
+    console.error(err);
+});
 
 // Connect to database and start listening for requests
 const mongoose = require('mongoose');
 mongoose.connect(
     process.env.MONGODB_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    },
     (err) => {
         if (!err)
             app.listen(process.env.PORT || 2020, () => console.log("Application is live."));
