7e87d2b976fa5d2ad366159ce7002bbb0d37f443
Joseph Edusei <jedusei99@gmail.com>
2020-05-18 11:55:50 +0000

    Added cors.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(require('cors')());
 require('./Routers').init(app);
 // Error handler
 app.use((err, req, res, next) => {
